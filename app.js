3c4287db9ec1148732d7a1961cc1892e3127304b
thapa technical <vbthapa55@gmail.com>
2025-01-09 16:31:41 +0530

    express url shorterner code

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
+import express from "express";
 import { readFile, writeFile } from "fs/promises";
-import { createServer } from "http";
 import crypto from "crypto";
 import path from "path";
+
+const app = express();
 
 const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join("data", "links.json");
 
-const serveFile = async (res, filePath, contentType) => {
-  try {
-    const data = await readFile(filePath);
-    res.writeHead(200, { "Content-Type": contentType });
-    res.end(data);
-  } catch (error) {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 page not found");
-  }
-};
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
 const loadLinks = async () => {
   try {
@@ -34,66 +28,64 @@ const saveLinks = async (links) => {
   await writeFile(DATA_FILE, JSON.stringify(links));
 };
 
-const server = createServer(async (req, res) => {
-  console.log(req.url);
-
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      return serveFile(res, path.join("public", "index.html"), "text/html");
-    } else if (req.url === "/style.css") {
-      return serveFile(res, path.join("public", "style.css"), "text/css");
-    } else if (req.url === "/links") {
-      const links = await loadLinks();
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      return res.end(JSON.stringify(links));
-    } else {
-      const links = await loadLinks();
-      const shortCode = req.url.slice(1);
-      console.log("links red. ", req.url);
-
-      if (links[shortCode]) {
-        res.writeHead(302, { location: links[shortCode] });
-        return res.end();
-      }
-
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      return res.end("Shortened URL is not found");
-    }
-  }
-
-  if (req.method === "POST" && req.url === "/shorten") {
+app.get("/", async (req, res) => {
+  try {
+    const file = await readFile(path.join("views", "index.html"));
     const links = await loadLinks();
 
-    let body = "";
-    req.on("data", (chunk) => (body += chunk));
-
-    req.on("end", async () => {
-      console.log(body);
-      const { url, shortCode } = JSON.parse(body);
-
-      if (!url) {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        return res.end("URL is required");
-      }
-
-      const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-
-      if (links[finalShortCode]) {
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        return res.end("Short code already exists. Please choose another.");
-      }
-
-      links[finalShortCode] = url;
-
-      await saveLinks(links);
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ success: true, shortCode: finalShortCode }));
-    });
+    const content = file.toString().replaceAll(
+      "{{ shortened_urls }}",
+      Object.entries(links)
+        .map(
+          ([shortCode, url]) =>
+            `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> - ${url}</li>`
+        )
+        .join("")
+    );
+    return res.send(content);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal server error");
   }
 });
 
-server.listen(PORT, () => {
+app.post("/", async (req, res) => {
+  try {
+    const { url, shortCode } = req.body;
+    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+    const links = await loadLinks();
+
+    if (links[finalShortCode]) {
+      return res
+        .status(400)
+        .send("Short code already exists. Please choose another.");
+    }
+
+    links[finalShortCode] = url;
+
+    await saveLinks(links);
+    return res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/:shortCode", async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    const links = await loadLinks();
+
+    if (!links[shortCode]) return res.status(404).send("404 error occurred");
+
+    return res.redirect(links[shortCode]);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
