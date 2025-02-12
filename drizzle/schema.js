1e78951a47b4c6d02b7a583ec09ebaef2d3bf1cc
thapa technical <vbthapa55@gmail.com>
2025-02-12 17:20:30 +0530

    user table schema added

--- drizzle/schema.js
+++ drizzle/schema.js
@@ -1,6 +1,16 @@
-import { int, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { int, mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+
 export const shortLinksTable = mysqlTable("short_link", {
   id: int().autoincrement().primaryKey(),
   url: varchar({ length: 255 }).notNull(),
   shortCode: varchar("short_code", { length: 20 }).notNull().unique(),
+});
+
+export const usersTable = mysqlTable("users", {
+  id: int().autoincrement().primaryKey(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().onUpdateNow().notNull(),
 });
